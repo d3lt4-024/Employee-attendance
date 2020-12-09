5d98952184cd81066971b572d7eb40b62de1c445
d3lt4-024 <phamhaivu2410@gmail.com>
2020-12-10 02:32:16 +0700

    Back-end core

---
+++ mvc/models/Admin.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class Admin extends User
+{
+    //this class use to fetch data from db for user admin
+    //but I am not complete db architecture for admin
+    //You can complete that and write code to this class
+}
